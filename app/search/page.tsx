4b9a0e033b383224981c67fe7bcb52c5771e32bc
antonbinom <a.semenikhin@vc.click>
2024-01-02 23:05:14 +0530

    create: pages and components

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,20 @@
+import NavBar from "../components/NavBar";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
 import RestaurantCard from "./components/RestaurantCard";
-import styles from "./page.module.css";
+import SearchSideBar from "./components/SearchSideBar";
 
-export default function Home() {
+export default function Search() {
 	return (
 		<main className="bg-gray-100 min-h-screen w-screen">
 			<main className="max-w-screen-2xl m-auto bg-white">
 				<NavBar />
-				<main>
-					<Header />
-					<div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+				<Header />
+				<div className="flex py-4 m-auto w-2/3 justify-between items-start">
+					<SearchSideBar />
+					<div className="w-5/6">
 						<RestaurantCard />
 					</div>
-				</main>
+				</div>
 			</main>
 		</main>
 	);
